c1a7d20596db2b0b4e40f61036b619c9f768efb7
smkn1waybungur <islamsyaiful217@gmail.com>
2026-09-21 21:05:56 +0700

    Create config.js for GURUWEB API configuration
    Added configuration file for GURUWEB with API URL.

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/*
+  GURUWEB GRATIS
+  Ganti URL di bawah ini dengan URL Web App Google Apps Script milik Anda.
+  Contoh: https://script.google.com/macros/s/AKfycbXXXXXXXX/exec
+*/
+window.GURUWEB_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbwk0TzBgLOzXGEqGF4WWzmp2wRLhUK7vvJY39XwwvKNxsfVlleH9FfZg1Uakr2kS14SjQ/exec"
+};
